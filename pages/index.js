e763a35b8b6e5be3c33d27279912706e05f0edf4
sunilksr <156302137+sunilksr@users.noreply.github.com>
2024-01-27 22:05:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,42 +7,43 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
-  const [notification, setNotification] = useState({ message: "", visible: false });
-  const [language, setLanguage] = useState("english"); 
-  const [amount, setAmount] = useState(0);
+  const [lockStatus, setLockStatus] = useState(false);
+  const [pin, setPin] = useState(6346); // Default pin
+  const [inputPin, setInputPin] = useState("");
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async () => {
+  const getWallet = async() => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({ method: "eth_accounts" });
+      const account = await ethWallet.request({method: "eth_accounts"});
       handleAccount(account);
     }
   }
 
   const handleAccount = (account) => {
     if (account) {
-      console.log("Account connected: ", account);
+      console.log ("Account connected: ", account);
       setAccount(account);
-    } else {
+    }
+    else {
       console.log("No account found");
     }
   }
 
-  const connectAccount = async () => {
+  const connectAccount = async() => {
     if (!ethWallet) {
       alert('MetaMask wallet is required to connect');
       return;
     }
-
+  
     const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
     handleAccount(accounts);
-
+    
     // once wallet is set we can get a reference to our deployed contract
     getATMContract();
   };
@@ -51,142 +52,59 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
-
+ 
     setATM(atmContract);
   }
 
-  const getBalance = async () => {
+  const getBalance = async() => {
     if (atm) {
       setBalance((await atm.getBalance()).toNumber());
     }
   }
 
-  const deposit = async () => {
-    if (atm) {
-      try {
-        let tx = await atm.deposit(amount);
-        await tx.wait();
-        getBalance();
-        showNotification(`Deposit successful, Amount: ${amount} ETH`, true, "Deposit");
-      } catch (error) {
-        console.error("Deposit error:", error);
-        showNotification("Deposit failed", false, "Deposit");
-      }
+  const deposit = async() => {
+    if (atm && inputPin === pin.toString()) {
+      let tx = await atm.deposit(1);
+      await tx.wait();
+      getBalance();
+    } else {
+      alert("Incorrect PIN");
     }
   }
 
-  const withdraw = async () => {
-    if (atm) {
-      try {
-        let tx = await atm.withdraw(amount);
-        await tx.wait();
-        getBalance();
-        showNotification(`Withdrawal successful, Amount: ${amount} ETH`, true, "Withdrawal");
-      } catch (error) {
-        console.error("Withdrawal error:", error);
-        showNotification("Withdrawal failed", false, "Withdrawal");
-      }
+  const withdraw = async() => {
+    if (atm && inputPin === pin.toString()) {
+      let tx = await atm.withdraw(1);
+      await tx.wait();
+      getBalance();
+    } else {
+      alert("Incorrect PIN");
     }
   }
 
-  const switchLanguage = (newLanguage) => {
-    setLanguage(newLanguage);
+  const toggleLock = async () => {
+    if (atm) {
+      await atm.toggleLock();
+      setLockStatus(!lockStatus);
+    }
   }
 
-  const showNotification = (message, success, type) => {
-    const currentDate = new Date();
-    const formattedDate = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()}`;
-
-    setNotification({ message: `${message} - ${formattedDate}`, success, type, visible: true });
-
-    // Hide the notification after a few seconds
-    setTimeout(() => {
-      setNotification({ message: "", success: false, type: "", visible: false });
-    }, 5000);
-  }
-
-  const renderLanguageButtons = () => {
-    return (
-      <div>
-        <button onClick={() => switchLanguage("hindi")}>हिंदी में</button>
-        <button onClick={() => switchLanguage("tamil")}>தமிழில்</button>
-        <button onClick={() => switchLanguage("english")}>English</button>
-      </div>
-    );
-  }
-
-  const renderLanguageSpecificContent = () => {
-    const languageContent = {
-      english: (
-        <div>
-          <p>Your Account: {account}</p>
-          <p>Your Balance: {balance}</p>
-          <label>
-            Amount:
-            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-          </label>
-          <button onClick={deposit}>Deposit</button>
-          <button onClick={withdraw}>Withdraw</button>
-          <p>Bank Name: My Bank</p>
-          <p>IFSC Code: ABCDE123456</p>
-          <p>PAN Number: ABCDE1234F</p>
-          <p>Account Holder Name:  Sunil KS</p>
-          <p>Education: Bachelor's Degree</p>
-        </div>
-      ),
-      hindi: (
-        <div>
-          <p>आपका खाता: {account}</p>
-          <p>आपका शेष: {balance}</p>
-          <label>
-            राशि:
-            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-          </label>
-          <button onClick={deposit}>जमा करें</button>
-          <button onClick={withdraw}>निकालें</button>
-          <p>बैंक का नाम: मेरा बैंक</p>
-          <p>IFSC कोड: एबीसीडी१२३४५६</p>
-          <p>PAN नंबर: एबीसीडीई१२३४एफ</p>
-          <p>खाता धारक का नाम: जॉन डो</p>
-          <p>शिक्षा: बैचलर्स डिग्री</p>
-        </div>
-      ),
-      tamil: (
-        <div>
-          <p>உங்கள் கணக்கு: {account}</p>
-          <p>உங்கள் மீது உள்ள மொத்தம்: {balance}</p>
-          <label>
-            தொகை:
-            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-          </label>
-          <button onClick={deposit}>வைத்திருக்கவும்</button>
-          <button onClick={withdraw}>எடுத்துக்கொள்ளவும்</button>
-          <p>வங்கி பெயர்: என் வங்கி</p>
-          <p>IFSC குறியீடு: ஏபிசிடி123456</p>
-          <p>PAN எண்: ஏபிசிடி1234எஃ</p>
-          <p>கணக்கு உரியாளரின் பெயர்: ஜான் டோ</p>
-          <p>கல்வி: பேச்லர்ஸ் டிகிரி</p>
-        </div>
-      ),
-    };
-
-    return languageContent[language] || languageContent.english;
+  const changePin = async (newPin) => {
+    if (atm) {
+      await atm.changePin(newPin);
+      setPin(newPin);
+    }
   }
 
   const initUser = () => {
-    // Check to see if the user has Metamask
+    // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask to use this ATM.</p>;
+      return <p>Please install Metamask in order to use this ATM.</p>
     }
 
-    // Check to see if the user is connected. If not, connect to their account
+    // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return (
-        <div>
-          {renderLanguageButtons()}
-          <button onClick={connectAccount}>मेटामास्क वॉलेट कनेक्ट करें</button>
-        </div>
-      );
+      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
     }
 
     if (balance === undefined) {
@@ -195,50 +113,37 @@ export default function HomePage() {
 
     return (
       <div>
-        {renderLanguageButtons()}
-        {renderLanguageSpecificContent()}
-
-        {/* Notification */}
-        {notification.visible && (
-          <div className={`notification ${notification.success ? 'success' : 'error'}`}>
-            {notification.message} {notification.type && `(${notification.type} ${amount} ETH)`}
-          </div>
-        )}
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance}</p>
+        <p>Owner Name: Sunil KS</p>
+        <p>Country: India</p>
+        <p>Date of Birth: 20 Jan 2002</p>
+        <input
+          type="text"
+          placeholder="Enter PIN"
+          value={inputPin}
+          onChange={(e) => setInputPin(e.target.value)}
+        />
+        <button style={{backgroundColor: "red"}} onClick={deposit}>Deposit 1 ETH</button>
+        <button style={{backgroundColor: "green"}} onClick={withdraw}>Withdraw 1 ETH</button>
+        <button style={{backgroundColor: "blue"}} onClick={toggleLock}>{lockStatus ? "Unlock Contract" : "Lock Contract"}</button>
+        <button style={{backgroundColor: "purple"}} onClick={() => changePin(5434)}>Change PIN to 5434</button>
       </div>
-    );
+    )
   }
 
-  useEffect(() => { getWallet(); }, []);
+  useEffect(() => {getWallet();}, []);
 
   return (
-    <main className="container">
-      <header><h1>{language === 'english' ? 'Welcome to the Metacrafters ATM!' : language === 'hindi' ? 'मेटाक्राफ्टर्स एटीएम में आपका स्वागत है!' : 'வருக Metacrafters ஏடிஎம்!'}</h1></header>
+    <main className="container" style={{backgroundColor: "yellow"}}>
+      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
       {initUser()}
       <style jsx>{`
         .container {
-          text-align: center;
-          background-color: yellow; /* Yellow Background */
-        }
-
-        .notification {
-          position: fixed;
-          top: 20px;
-          right: 20px;
-          padding: 10px;
-          border-radius: 5px;
-          color: #fff;
-          font-weight: bold;
-        }
-
-        .success {
-          background-color: #4CAF50;
-        }
-
-        .error {
-          background-color: #f44336;
+          text-align: center
         }
       `}
       </style>
     </main>
-  );
+  )
 }
