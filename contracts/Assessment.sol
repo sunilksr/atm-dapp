3f42c80d0ad9f7489833ead12032a71d4b573bca
sunilksr <156302137+sunilksr@users.noreply.github.com>
2024-01-27 22:05:24 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -4,58 +4,68 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-    mapping(address => uint256) public cryptoHoldings;
+    bool public locked;
+    uint256 public pin;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event CreditScoreUpdated(address indexed account, uint256 newCreditScore);
-    event DebitCardIssued(address indexed account, uint256 cardNumber);
-    event CreditCardIssued(address indexed account, uint256 cardNumber);
+    event LockStatusChanged(bool newStatus);
+    event PinChanged(uint256 newPin);
 
-    constructor(uint256 initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
     }
 
-    function getBalance() public view returns(uint256) {
+    modifier notLocked() {
+        require(!locked, "Contract is locked");
+        _;
+    }
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+        locked = false;
+        pin = 6436; // Default pin
+    }
+
+    function getBalance() public view returns(uint256){
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function deposit(uint256 _amount) public payable notLocked {
+        // perform transaction
         balance += _amount;
+
+        // emit the event
         emit Deposit(_amount);
     }
 
+    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function withdraw(uint256 _withdrawAmount) public onlyOwner notLocked {
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
                 withdrawAmount: _withdrawAmount
             });
         }
+
+        // withdraw the given amount
         balance -= _withdrawAmount;
+
+        // emit the event
         emit Withdraw(_withdrawAmount);
     }
 
-    function updateCreditScore(address _account, uint256 _newCreditScore) public {
-        require(msg.sender == owner, "You are not authorized to update credit scores");
-        cryptoHoldings[_account] = _newCreditScore;
-        emit CreditScoreUpdated(_account, _newCreditScore);
+    function toggleLock() public onlyOwner {
+        locked = !locked;
+        emit LockStatusChanged(locked);
     }
 
-    function issueDebitCard(address _account) public {
-        require(msg.sender == owner, "You are not authorized to issue debit cards");
-        uint256 cardNumber = uint256(keccak256(abi.encodePacked(block.timestamp, _account))) % 10000000000000000;
-        emit DebitCardIssued(_account, cardNumber);
-    }
-
-    function issueCreditCard(address _account) public {
-        require(msg.sender == owner, "You are not authorized to issue credit cards");
-        uint256 cardNumber = uint256(keccak256(abi.encodePacked(block.timestamp, _account))) % 10000000000000000;
-        emit CreditCardIssued(_account, cardNumber);
+    function changePin(uint256 _newPin) public onlyOwner {
+        pin = _newPin;
+        emit PinChanged(_newPin);
     }
 }
